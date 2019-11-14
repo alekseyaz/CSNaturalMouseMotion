48e646c3cba1658160f6646bef2906aaff681bb1
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-11-14 15:11:52 +0300

    add ISystemCalls

---
+++ CSNaturalMouseMotion/Interface/ISystemCalls.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalMouseMotion.Interface
+{
+    public interface ISystemCalls
+    {
+        long currentTimeMillis();
+        void sleep(long time);
+        java.awt.Dimension getScreenSize();
+        void setMousePosition(int x, int y);
+    }
+}
