3637e21b3ab04732c2520eeb294899fd86137259
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-11-16 14:16:12 +0300

    add IDeviationProvider

---
+++ CSNaturalMouseMotion/Interface/IDeviationProvider.cs
@@ -0,0 +1,10 @@
+﻿using NaturalMouseMotion.Support;
+using System.Drawing;
+
+namespace NaturalMouseMotion.Interface
+{
+    public interface IDeviationProvider
+    {
+        DoublePoint getDeviation(double totalDistanceInPixels, double completionFraction);
+    }
+}
