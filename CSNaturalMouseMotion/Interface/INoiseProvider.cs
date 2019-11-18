4378262f5b40bbfc13dafcb7bd44fcf03f0b7e1a
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-11-18 19:16:12 +0300

    add INoiseProvider

---
+++ CSNaturalMouseMotion/Interface/INoiseProvider.cs
@@ -0,0 +1,13 @@
+﻿using NaturalMouseMotion.Support;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace NaturalMouseMotion.Interface
+{
+    public interface INoiseProvider
+    {
+        DoublePoint getNoise(Random random, double xStepSize, double yStepSize);
+    }
+}
