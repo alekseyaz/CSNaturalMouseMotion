7a6fe1d69029e382841d60be68b1985d6f9e64fc
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-11-19 19:14:42 +0300

    add IOvershootManager

---
+++ CSNaturalMouseMotion/Interface/IOvershootManager.cs
@@ -0,0 +1,14 @@
+﻿using NaturalMouseMotion.Support;
+using System.Drawing;
+
+namespace NaturalMouseMotion.Interface
+{
+    public interface IOvershootManager
+    {
+        int getOvershoots(Flow flow, long mouseMovementMs, double distance);
+
+        Point getOvershootAmount(double distanceToRealTargetX, double distanceToRealTargetY, long mouseMovementMs, int overshootsRemaining);
+
+        long deriveNextMouseMovementTimeMs(long mouseMovementMs, int overshootsRemaining);
+    }
+}
