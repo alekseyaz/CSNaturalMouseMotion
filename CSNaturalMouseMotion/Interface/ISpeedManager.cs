19543c01aaef1791797650cd8e2cd1e4c0ab5099
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-11-25 19:13:25 +0300

    add ISpeedManager

---
+++ CSNaturalMouseMotion/Interface/ISpeedManager.cs
@@ -0,0 +1,13 @@
+﻿using CSNaturalMouseMotion.Util;
+using NaturalMouseMotion.Support;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalMouseMotion.Interface
+{
+    public interface ISpeedManager
+    {
+        Pair<Flow, long> getFlowWithTime(double distance);
+    }
+}
