9fc2e72ce9533bc1b0d9e69a6d07b05911024197
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-12-01 13:53:34 +0300

    Нжен DoublePoint

---
+++ CSNaturalMouseMotion/Support/DoublePoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalMouseMotion.Support
+{
+    public class DoublePoint
+    {
+
+        public static DoublePoint ZERO = new DoublePoint(0, 0);
+
+        private double x;
+
+        private double y;
+
+        public DoublePoint(double x, double y)
+        {
+            this.x = this.x;
+            this.y = this.y;
+        }
+
+        public double getX()
+        {
+            return this.x;
+        }
+
+        public double getY()
+        {
+            return this.y;
+        }
+    }
+}
