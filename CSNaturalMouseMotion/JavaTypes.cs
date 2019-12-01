4b1f0a467e6b6f4024c6b54f75c186fa44a545a2
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-12-01 22:16:51 +0300

    java тип hypot и Pair

---
+++ CSNaturalMouseMotion/JavaTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java
+{
+    public static class Math
+    {
+        public static double hypot(double x, double y)
+        {
+            return System.Math.Sqrt(x * x + y * y);
+        }
+        public static double random() => new Random().NextDouble();
+
+    }
+
+    public static class Pair<D, Y>
+    {
+        public static D x;
+        public static Y y;
+    }
+
+}
