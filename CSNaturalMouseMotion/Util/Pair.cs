2b06de50d371a7e9af56f8508aa95b9f0408c2f5
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-12-01 18:26:44 +0300

    Нужен Pair

---
+++ CSNaturalMouseMotion/Util/Pair.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSNaturalMouseMotion.Util
+{
+    public class Pair<X, Y>
+    {
+
+        public X x;
+
+        public Y y;
+
+        public Pair(X x, Y y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
