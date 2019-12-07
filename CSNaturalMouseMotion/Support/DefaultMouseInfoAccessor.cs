725d5656074dbfcd31de24c667e2de3d0b2b4def
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2019-12-07 15:17:24 +1300

    Реализации интерфейсов IMouseInfoAccessor, IOvershootManager

---
+++ CSNaturalMouseMotion/Support/DefaultMouseInfoAccessor.cs
@@ -0,0 +1,16 @@
+﻿using NaturalMouseMotion.Interface;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace NaturalMouseMotion.Support
+{
+    public class DefaultMouseInfoAccessor : IMouseInfoAccessor
+    {
+        public Point getMousePosition()
+        {
+            return MouseInfo.getPointerInfo().getLocation();
+        }
+    }
+}
---
+++ CSNaturalMouseMotion/Support/DefaultMouseMotionNature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NaturalMouseMotion.Support
+{
+    public class DefaultMouseMotionNature : MouseMotionNature
+    {
+
+        public static int TIME_TO_STEPS_DIVIDER = 8;
+
+        public static int MIN_STEPS = 10;
+
+        public static int EFFECT_FADE_STEPS = 15;
+
+        public static int REACTION_TIME_BASE_MS = 20;
+
+        public static int REACTION_TIME_VARIATION_MS = 120;
+
+        public DefaultMouseMotionNature()
+        {
+            try
+            {
+                setSystemCalls(new DefaultSystemCalls(new Robot()));
+            }
+            catch (AWTException e)
+            {
+                throw new RuntimeException(e);
+            }
+
+            setDeviationProvider(new SinusoidalDeviationProvider(DEFAULT_SLOPE_DIVIDER));
+            setNoiseProvider(new DefaultNoiseProvider(DEFAULT_NOISINESS_DIVIDER));
+            setSpeedManager(new DefaultSpeedManager());
+            setOvershootManager(new DefaultOvershootManager(new Random()));
+            setEffectFadeSteps(EFFECT_FADE_STEPS);
+            setMinSteps(MIN_STEPS);
+            setMouseInfo(new DefaultMouseInfoAccessor());
+            setReactionTimeBaseMs(REACTION_TIME_BASE_MS);
+            setReactionTimeVariationMs(REACTION_TIME_VARIATION_MS);
+            setTimeToStepsDivider(TIME_TO_STEPS_DIVIDER);
+        }
+    }
+}
