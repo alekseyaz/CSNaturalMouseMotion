25c60b6fe9a96fef44f58fc39a46a32cca3f2975
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-01-19 13:17:25 +0300

    Добавим немного тестов

---
+++ NaturalMouseMotion.Tests/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NaturalMouseMotion.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MouseMotionFactory factory;
+            factory = new MouseMotionFactory();
+
+            MouseMotion motion = factory.build(350, 450);
+            MouseMotion motionBack = factory.build(1900, 1000);
+
+            for (int i = 0; i < 5; ++i)
+            {
+                motion.move();
+                Thread.Sleep(50);
+                motionBack.move();
+                Thread.Sleep(50);
+            }
+        }
+    }
+}
