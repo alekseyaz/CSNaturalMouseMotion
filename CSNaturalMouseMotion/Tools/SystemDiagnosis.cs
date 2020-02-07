19a57aa1bd85dd8c7dac5a7f9b4c7fcbb171ff0d
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-02-07 16:24:14 +0300

    ...

--- CSNaturalMouseMotion/Tools/SystemDiagnosis.cs
+++ CSNaturalMouseMotion/Tools/SystemDiagnosis.cs
@@ -8,55 +8,66 @@ using System.Threading;
 
 namespace NaturalMouseMotion.Tools
 {
-    public class SystemDiagnosis
-    {
+	public class SystemDiagnosis
+	{
 
-        public static void validateMouseMovement()
-        {
-            try
-            {
-                java.awt.Robot robot = new java.awt.Robot();
-                SystemDiagnosis.validateMouseMovement(new DefaultSystemCalls(robot), new DefaultMouseInfoAccessor());
-            }
-            catch (java.awt.AWTException e)
-            {
-                throw new RuntimeException(e);
-            }
+		/// <summary>
+		/// Runs a diagnosis with default configuration, by setting mouse all over your screen and expecting to receive
+		/// correct coordinates back.
+		/// If java.awt.Robot cannot be constructed, then new RuntimeException is thrown.
+		/// If no issues are found, then this method completes without throwing an error, otherwise IllegalStateException is
+		/// thrown.
+		/// </summary>
+		public static void validateMouseMovement()
+		{
+			try
+			{
+				Robot robot = new Robot();
+				validateMouseMovement(new DefaultSystemCalls(robot), new DefaultMouseInfoAccessor());
+			}
+			catch (AWTException e)
+			{
+				throw new Exception(e);
+			}
+		}
 
-        }
+		/// <summary>
+		/// Runs a diagnosis, by setting mouse all over your screen and expecting to receive correct coordinates back.
+		/// If no issues are found, then this method completes without throwing an error, otherwise IllegalStateException is
+		/// thrown. </summary>
+		/// <param name="system"> a SystemCalls class which is used for setting the mouse position </param>
+		/// <param name="accessor"> a MouseInfoAccessor which is used for querying mouse position </param>
+		public static void validateMouseMovement(ISystemCalls system, IMouseInfoAccessor accessor)
+		{
+			Dimension dimension = system.ScreenSize;
+			for (int y = 0; y < dimension.height; y += 50)
+			{
+				for (int x = 0; x < dimension.width; x += 50)
+				{
+					system.setMousePosition(x, y);
 
-        public static void validateMouseMovement(ISystemCalls system, IMouseInfoAccessor accessor)
-        {
-            java.awt.Dimension dimension = system.getScreenSize();
-            for (int y = 0; (y < dimension.height); y += 50)
-            {
-                for (int x = 0; (x < dimension.width); x += 50)
-                {
-                    system.setMousePosition(x, y);
-                    try
-                    {
-                        Thread.Sleep(1);
-                    }
-                    catch (InterruptedException e)
-                    {
-                        Thread.CurrentThread.Interrupt();
-                    }
+					try
+					{
+						Thread.Sleep(1);
+					}
+					catch (ThreadInterruptedException)
+					{
+						Thread.CurrentThread.Interrupt();
+					}
 
-                    Point p = accessor.getMousePosition();
-                    if (((x != p.X)
-                                || (y != p.y)))
-                    {
-                        throw new IllegalStateException(("Tried to move mouse to ("
-                                        + (x + (", "
-                                        + (y + ("). Actually moved to ("
-                                        + (p.X + (", "
-                                        + (p.Y + (")" + ("This means NaturalMouseMotion is not able to work optimally on this system as the cursor move " + "calls may miss the target pixels on the screen.")))))))))));
-                    }
-
-                }
-
-            }
-
-        }
-    }
+					Point p = accessor.MousePosition;
+					if (x != p.X || y != p.Y)
+					{
+						throw new System.InvalidOperationException("Tried to move mouse to (" + x
+							+ ", " + y
+							+ "). Actually moved to (" + p.X
+							+ ", " + p.Y
+							+ ")"
+							+ "This means NaturalMouseMotion is not able to work optimally on this system as the cursor move "
+							+ "calls may miss the target pixels on the screen.");
+					}
+				}
+			}
+		}
+	}
 }
