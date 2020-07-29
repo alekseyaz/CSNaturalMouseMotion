19e514cecdf52d8bf12d97ab9e794ba634884b9e
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-07-29 22:41:11 +0300

    ...

--- CSNaturalMouseMotion/Util/FactoryTemplates.cs
+++ CSNaturalMouseMotion/Util/FactoryTemplates.cs
@@ -1,4 +1,6 @@
-﻿using NaturalMouseMotion;
+﻿//using CSNaturalMouseMotion.Support;
+using CSNaturalMouseMotion.TestUtils;
+using NaturalMouseMotion;
 using NaturalMouseMotion.Interface;
 using NaturalMouseMotion.Support;
 using System;
@@ -10,147 +12,164 @@ using System.Threading.Tasks;
 
 namespace CSNaturalMouseMotion.Util
 {
-	public class FactoryTemplates
-	{
-		/// <summary>
-		/// <h1>Stereotypical granny using a computer with non-optical mouse from the 90s.</h1>
-		/// Low speed, variating flow, lots of noise in movement.
-		/// </summary>
-		/// <returns> the factory </returns>
-		public static MouseMotionFactory createGrannyMotionFactory()
-		{
-			return createGrannyMotionFactory(new DefaultMouseMotionNature());
-		}
+    public class FactoryTemplates
+    {
+        /// <summary>
+        /// <h1>Stereotypical granny using a computer with non-optical mouse from the 90s.</h1>
+        /// Low speed, variating flow, lots of noise in movement.
+        /// </summary>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createGrannyMotionFactory()
+        {
+            return createGrannyMotionFactory(new DefaultMouseMotionNature());
+        }
 
-		/// <summary>
-		/// <h1>Stereotypical granny using a computer with non-optical mouse from the 90s.</h1>
-		/// Low speed, variating flow, lots of noise in movement. </summary>
-		/// <param name="nature"> the nature for the template to be configured on </param>
-		/// <returns> the factory </returns>
-		public static MouseMotionFactory createGrannyMotionFactory(MouseMotionNature nature)
-		{
-			MouseMotionFactory factory = new MouseMotionFactory(nature);
-			IList<Flow> flows = new List<Flow> { new Flow(FlowTemplates.jaggedFlow()), new Flow(FlowTemplates.random()), new Flow(FlowTemplates.interruptedFlow()), new Flow(FlowTemplates.interruptedFlow2()), new Flow(FlowTemplates.adjustingFlow()), new Flow(FlowTemplates.stoppingFlow()) };
+        /// <summary>
+        /// <h1>Stereotypical granny using a computer with non-optical mouse from the 90s.</h1>
+        /// Low speed, variating flow, lots of noise in movement. </summary>
+        /// <param name="nature"> the nature for the template to be configured on </param>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createGrannyMotionFactory(MouseMotionNature nature)
+        {
+            MouseMotionFactory factory = new MouseMotionFactory(nature);
+            IList<Flow> flows = new List<Flow> {
+                new Flow(FlowTemplates.jaggedFlow()),
+                new Flow(FlowTemplates.random()),
+                new Flow(FlowTemplates.interruptedFlow()),
+                new Flow(FlowTemplates.interruptedFlow2()),
+                new Flow(FlowTemplates.adjustingFlow()),
+                new Flow(FlowTemplates.stoppingFlow())
+            };
 
-			DefaultSpeedManager manager = new DefaultSpeedManager(flows);
-			factory.DeviationProvider = new SinusoidalDeviationProvider(9);
-			factory.NoiseProvider = new DefaultNoiseProvider(1.6);
-			factory.Nature.ReactionTimeBaseMs = 100;
+            DefaultSpeedManager manager = new DefaultSpeedManager(flows);
+            factory.DeviationProvider = new SinusoidalDeviationProvider(9);
+            factory.NoiseProvider = new DefaultNoiseProvider(1.6);
+            factory.Nature.ReactionTimeBaseMs = 100;
 
-			DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
-			overshootManager.Overshoots = 3;
-			overshootManager.MinDistanceForOvershoots = 3;
-			overshootManager.MinOvershootMovementMs = 400;
-			overshootManager.OvershootRandomModifierDivider = DefaultOvershootManager.OVERSHOOT_RANDOM_MODIFIER_DIVIDER / 2;
-			overshootManager.OvershootSpeedupDivider = DefaultOvershootManager.OVERSHOOT_SPEEDUP_DIVIDER * 2;
+            DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
+            overshootManager.Overshoots = 3;
+            overshootManager.MinDistanceForOvershoots = 3;
+            overshootManager.MinOvershootMovementMs = 400;
+            overshootManager.OvershootRandomModifierDivider = DefaultOvershootManager.OVERSHOOT_RANDOM_MODIFIER_DIVIDER / 2;
+            overshootManager.OvershootSpeedupDivider = DefaultOvershootManager.OVERSHOOT_SPEEDUP_DIVIDER * 2;
 
-			factory.Nature.TimeToStepsDivider = DefaultMouseMotionNature.TIME_TO_STEPS_DIVIDER - 2;
-			manager.MouseMovementBaseTimeMs = 1000;
-			factory.SpeedManager = manager;
-			return factory;
-		}
+            factory.Nature.TimeToStepsDivider = DefaultMouseMotionNature.TIME_TO_STEPS_DIVIDER - 2;
+            manager.MouseMovementBaseTimeMs = 1000;
+            factory.SpeedManager = manager;
+            return factory;
+        }
 
-		/// <summary>
-		/// <h1>Robotic fluent movement.</h1>
-		/// Custom speed, constant movement, no mistakes, no overshoots.
-		/// </summary>
-		/// <param name="motionTimeMsPer100Pixels"> approximate time a movement takes per 100 pixels of travelling </param>
-		/// <returns> the factory </returns>
-		//public static MouseMotionFactory createDemoRobotMotionFactory(long motionTimeMsPer100Pixels)
-		//{
-		//	return createDemoRobotMotionFactory(new DefaultMouseMotionNature(), motionTimeMsPer100Pixels);
-		//}
+        /// <summary>
+        /// <h1>Robotic fluent movement.</h1>
+        /// Custom speed, constant movement, no mistakes, no overshoots.
+        /// </summary>
+        /// <param name="motionTimeMsPer100Pixels"> approximate time a movement takes per 100 pixels of travelling </param>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createDemoRobotMotionFactory(long motionTimeMsPer100Pixels)
+        {
+            return createDemoRobotMotionFactory(new DefaultMouseMotionNature(), motionTimeMsPer100Pixels);
+        }
+        
+        /// <summary>
+        /// <h1>Robotic fluent movement.</h1>
+        /// Custom speed, constant movement, no mistakes, no overshoots.
+        /// </summary>
+        /// <param name="nature"> the nature for the template to be configured on </param>
+        /// <param name="motionTimeMsPer100Pixels"> approximate time a movement takes per 100 pixels of travelling </param>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createDemoRobotMotionFactory(MouseMotionNature nature, long motionTimeMsPer100Pixels)
+        {
+            MouseMotionFactory factory = new MouseMotionFactory(nature);
+            Flow flow = new Flow(FlowTemplates.constantSpeed());
+            double timePerPixel = motionTimeMsPer100Pixels / 100d;
+            //ISpeedManager manager = distance => new Pair<Flow, long>(flow, (long)(timePerPixel * distance));
+            ISpeedManager manager = new MockSpeedManager(flow, timePerPixel);
+            //factory.DeviationProvider = (totalDistanceInPixels, completionFraction) => DoublePoint.ZERO;
+            //factory.NoiseProvider = ((random, xStepSize, yStepSize) => DoublePoint.ZERO);
 
-		/// <summary>
-		/// <h1>Robotic fluent movement.</h1>
-		/// Custom speed, constant movement, no mistakes, no overshoots.
-		/// </summary>
-		/// <param name="nature"> the nature for the template to be configured on </param>
-		/// <param name="motionTimeMsPer100Pixels"> approximate time a movement takes per 100 pixels of travelling </param>
-		/// <returns> the factory </returns>
-		//public static MouseMotionFactory createDemoRobotMotionFactory(MouseMotionNature nature, long motionTimeMsPer100Pixels)
-		//{
-		//	MouseMotionFactory factory = new MouseMotionFactory(nature);
-		//	//JAVA TO C# CONVERTER WARNING: The original Java variable was marked 'final':
-		//	//ORIGINAL LINE: final com.github.joonasvali.naturalmouse.support.Flow flow = new com.github.joonasvali.naturalmouse.support.Flow(FlowTemplates.constantSpeed());
-		//	Flow flow = new Flow(FlowTemplates.constantSpeed());
+            DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
+            overshootManager.Overshoots = 0;
 
+            factory.SpeedManager = manager;
+            return factory;
+        }
 
+        /// <summary>
+        /// <h1>Gamer with fast reflexes and quick mouse movements.</h1>
+        /// Quick movement, low noise, some deviation, lots of overshoots.
+        /// </summary>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createFastGamerMotionFactory()
+        {
+            return createFastGamerMotionFactory(new DefaultMouseMotionNature());
+        }
 
+        /// <summary>
+        /// <h1>Gamer with fast reflexes and quick mouse movements.</h1>
+        /// Quick movement, low noise, some deviation, lots of overshoots. </summary>
+        /// <param name="nature"> the nature for the template to be configured on </param>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createFastGamerMotionFactory(MouseMotionNature nature)
+        {
+            MouseMotionFactory factory = new MouseMotionFactory(nature);
+            IList<Flow> flows = new List<Flow> {
+                new Flow(FlowTemplates.variatingFlow()),
+                new Flow(FlowTemplates.slowStartupFlow()),
+                new Flow(FlowTemplates.slowStartup2Flow()),
+                new Flow(FlowTemplates.adjustingFlow()),
+                new Flow(FlowTemplates.jaggedFlow())
+            };
+            DefaultSpeedManager manager = new DefaultSpeedManager(flows);
+            factory.DeviationProvider = new SinusoidalDeviationProvider(SinusoidalDeviationProvider.DEFAULT_SLOPE_DIVIDER);
+            factory.NoiseProvider = new DefaultNoiseProvider(DefaultNoiseProvider.DEFAULT_NOISINESS_DIVIDER);
+            factory.Nature.ReactionTimeVariationMs = 100;
+            manager.MouseMovementBaseTimeMs = 250;
 
-		//	double timePerPixel = motionTimeMsPer100Pixels / 100d;
-		//	ISpeedManager manager = distance => new Pair<Flow, long>(flow, (long)(timePerPixel * distance));
-		//	factory.DeviationProvider = (totalDistanceInPixels, completionFraction) => DoublePoint.ZERO;
-		//	factory.NoiseProvider = ((random, xStepSize, yStepSize) => DoublePoint.ZERO);
+            DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
+            overshootManager.Overshoots = 4;
 
-		//	DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
-		//	overshootManager.Overshoots = 0;
+            factory.SpeedManager = manager;
+            return factory;
+        }
+        /// <summary>
+        /// <h1>Standard computer user with average speed and movement mistakes</h1>
+        /// medium noise, medium speed, medium noise and deviation.
+        /// </summary>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createAverageComputerUserMotionFactory()
+        {
+            return createAverageComputerUserMotionFactory(new DefaultMouseMotionNature());
+        }
+        /// <summary>
+        /// <h1>Standard computer user with average speed and movement mistakes</h1>
+        /// medium noise, medium speed, medium noise and deviation.
+        /// </summary>
+        /// <param name="nature"> the nature for the template to be configured on </param>
+        /// <returns> the factory </returns>
+        public static MouseMotionFactory createAverageComputerUserMotionFactory(MouseMotionNature nature)
+        {
+            MouseMotionFactory factory = new MouseMotionFactory(nature);
+            IList<Flow> flows = new List<Flow> {
+                new Flow(FlowTemplates.variatingFlow()),
+                new Flow(FlowTemplates.interruptedFlow()),
+                new Flow(FlowTemplates.interruptedFlow2()),
+                new Flow(FlowTemplates.slowStartupFlow()),
+                new Flow(FlowTemplates.slowStartup2Flow()),
+                new Flow(FlowTemplates.adjustingFlow()),
+                new Flow(FlowTemplates.jaggedFlow()),
+                new Flow(FlowTemplates.stoppingFlow())
+            };
+            DefaultSpeedManager manager = new DefaultSpeedManager(flows);
+            factory.DeviationProvider = new SinusoidalDeviationProvider(SinusoidalDeviationProvider.DEFAULT_SLOPE_DIVIDER);
+            factory.NoiseProvider = new DefaultNoiseProvider(DefaultNoiseProvider.DEFAULT_NOISINESS_DIVIDER);
+            factory.Nature.ReactionTimeVariationMs = 110;
+            manager.MouseMovementBaseTimeMs = 400;
 
-		//	factory.SpeedManager = manager;
-		//	return factory;
-		//}
+            DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
+            overshootManager.Overshoots = 4;
 
-		/// <summary>
-		/// <h1>Gamer with fast reflexes and quick mouse movements.</h1>
-		/// Quick movement, low noise, some deviation, lots of overshoots.
-		/// </summary>
-		/// <returns> the factory </returns>
-		public static MouseMotionFactory createFastGamerMotionFactory()
-		{
-			return createFastGamerMotionFactory(new DefaultMouseMotionNature());
-		}
-
-		/// <summary>
-		/// <h1>Gamer with fast reflexes and quick mouse movements.</h1>
-		/// Quick movement, low noise, some deviation, lots of overshoots. </summary>
-		/// <param name="nature"> the nature for the template to be configured on </param>
-		/// <returns> the factory </returns>
-		public static MouseMotionFactory createFastGamerMotionFactory(MouseMotionNature nature)
-		{
-			MouseMotionFactory factory = new MouseMotionFactory(nature);
-			IList<Flow> flows = new List<Flow>{ new Flow(FlowTemplates.variatingFlow()), new Flow(FlowTemplates.slowStartupFlow()), new Flow(FlowTemplates.slowStartup2Flow()), new Flow(FlowTemplates.adjustingFlow()), new Flow(FlowTemplates.jaggedFlow()) };
-			DefaultSpeedManager manager = new DefaultSpeedManager(flows);
-			factory.DeviationProvider = new SinusoidalDeviationProvider(SinusoidalDeviationProvider.DEFAULT_SLOPE_DIVIDER);
-			factory.NoiseProvider = new DefaultNoiseProvider(DefaultNoiseProvider.DEFAULT_NOISINESS_DIVIDER);
-			factory.Nature.ReactionTimeVariationMs = 100;
-			manager.MouseMovementBaseTimeMs = 250;
-
-			DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
-			overshootManager.Overshoots = 4;
-
-			factory.SpeedManager = manager;
-			return factory;
-		}
-		/// <summary>
-		/// <h1>Standard computer user with average speed and movement mistakes</h1>
-		/// medium noise, medium speed, medium noise and deviation.
-		/// </summary>
-		/// <returns> the factory </returns>
-		public static MouseMotionFactory createAverageComputerUserMotionFactory()
-		{
-			return createAverageComputerUserMotionFactory(new DefaultMouseMotionNature());
-		}
-		/// <summary>
-		/// <h1>Standard computer user with average speed and movement mistakes</h1>
-		/// medium noise, medium speed, medium noise and deviation.
-		/// </summary>
-		/// <param name="nature"> the nature for the template to be configured on </param>
-		/// <returns> the factory </returns>
-		public static MouseMotionFactory createAverageComputerUserMotionFactory(MouseMotionNature nature)
-		{
-			MouseMotionFactory factory = new MouseMotionFactory(nature);
-			IList<Flow> flows = new List<Flow> { new Flow(FlowTemplates.variatingFlow()), new Flow(FlowTemplates.interruptedFlow()), new Flow(FlowTemplates.interruptedFlow2()), new Flow(FlowTemplates.slowStartupFlow()), new Flow(FlowTemplates.slowStartup2Flow()), new Flow(FlowTemplates.adjustingFlow()), new Flow(FlowTemplates.jaggedFlow()), new Flow(FlowTemplates.stoppingFlow()) };
-			DefaultSpeedManager manager = new DefaultSpeedManager(flows);
-			factory.DeviationProvider = new SinusoidalDeviationProvider(SinusoidalDeviationProvider.DEFAULT_SLOPE_DIVIDER);
-			factory.NoiseProvider = new DefaultNoiseProvider(DefaultNoiseProvider.DEFAULT_NOISINESS_DIVIDER);
-			factory.Nature.ReactionTimeVariationMs = 110;
-			manager.MouseMovementBaseTimeMs = 400;
-
-			DefaultOvershootManager overshootManager = (DefaultOvershootManager)factory.OvershootManager;
-			overshootManager.Overshoots = 4;
-
-			factory.SpeedManager = manager;
-			return factory;
-		}
-	}
+            factory.SpeedManager = manager;
+            return factory;
+        }
+    }
 }
--- CSNaturalMouseMotion/Util/FlowUtil.cs
+++ CSNaturalMouseMotion/Util/FlowUtil.cs
@@ -6,126 +6,123 @@ using System.Threading.Tasks;
 
 namespace CSNaturalMouseMotion.Util
 {
-	public class FlowUtil
-	{
-		/// <summary>
-		/// Stretch flow to longer length. Tries to fill the caps with averages.
-		/// 
-		/// This is an unintuitive method, because it turns out that, for example, array size of 3
-		/// scales better to array size of 5 than it does to array size of 6. [1, 2, 3] can be
-		/// easily scaled to [1, 1.5, 2, 2.5, 3], but it's not possible without recalculating middle number (2)
-		/// with array size of 6, simplistic solutions quickly would run to trouble like this  [1, 1.5, 2, 2.5, 3, (3)? ]
-		/// or maybe: [1, 1.5, 2, 2.5, ..., 3 ]. The correct solution would correctly scale the middle numbers </summary>
-		/// <param name="flow"> the original flow </param>
-		/// <param name="targetLength"> the resulting flow length </param>
-		/// <returns> the resulting flow </returns>
-		public static double[] stretchFlow(double[] flow, int targetLength)
-		{
-			return stretchFlow(flow, targetLength, a => a);
-		}
+    public class FlowUtil
+    {
+        /// <summary>
+        /// Stretch flow to longer length. Tries to fill the caps with averages.
+        /// 
+        /// This is an unintuitive method, because it turns out that, for example, array size of 3
+        /// scales better to array size of 5 than it does to array size of 6. [1, 2, 3] can be
+        /// easily scaled to [1, 1.5, 2, 2.5, 3], but it's not possible without recalculating middle number (2)
+        /// with array size of 6, simplistic solutions quickly would run to trouble like this  [1, 1.5, 2, 2.5, 3, (3)? ]
+        /// or maybe: [1, 1.5, 2, 2.5, ..., 3 ]. The correct solution would correctly scale the middle numbers </summary>
+        /// <param name="flow"> the original flow </param>
+        /// <param name="targetLength"> the resulting flow length </param>
+        /// <returns> the resulting flow </returns>
+        public static double[] stretchFlow(double[] flow, int targetLength)
+        {
+            return stretchFlow(flow, targetLength, a => a);
+        }
 
-		/// <summary>
-		/// Stretch flow to longer length. Tries to fill the caps with averages.
-		/// 
-		/// This is an unintuitive method, because it turns out that, for example, array size of 3
-		/// scales better to array size of 5 than it does to array size of 6. [1, 2, 3] can be
-		/// easily scaled to [1, 1.5, 2, 2.5, 3], but it's not possible without recalculating middle number (2)
-		/// with array size of 6, simplistic solutions quickly would run to trouble like this  [1, 1.5, 2, 2.5, 3, (3)? ]
-		/// or maybe: [1, 1.5, 2, 2.5, ..., 3 ]. The correct solution would correctly scale the middle numbers
-		/// over several indexes. </summary>
-		/// <param name="flow"> the original flow </param>
-		/// <param name="targetLength"> the resulting flow length </param>
-		/// <param name="modifier"> modifies the resulting values, you can use this to provide noise or amplify
-		///                 the flow characteristics. </param>
-		/// <returns> the resulting flow </returns>
-		public static double[] stretchFlow(double[] flow, int targetLength, System.Func<double, double> modifier)
-		{
-			if (targetLength < flow.Length)
-			{
-				throw new System.ArgumentException("Target bucket length smaller than flow. "
-					+ "" + targetLength
-					+ " vs " + flow.Length);
-			}
-			double[] result;
-			int tempLength = targetLength;
+        /// <summary>
+        /// Stretch flow to longer length. Tries to fill the caps with averages.
+        /// 
+        /// This is an unintuitive method, because it turns out that, for example, array size of 3
+        /// scales better to array size of 5 than it does to array size of 6. [1, 2, 3] can be
+        /// easily scaled to [1, 1.5, 2, 2.5, 3], but it's not possible without recalculating middle number (2)
+        /// with array size of 6, simplistic solutions quickly would run to trouble like this  [1, 1.5, 2, 2.5, 3, (3)? ]
+        /// or maybe: [1, 1.5, 2, 2.5, ..., 3 ]. The correct solution would correctly scale the middle numbers
+        /// over several indexes. </summary>
+        /// <param name="flow"> the original flow </param>
+        /// <param name="targetLength"> the resulting flow length </param>
+        /// <param name="modifier"> modifies the resulting values, you can use this to provide noise or amplify
+        ///                 the flow characteristics. </param>
+        /// <returns> the resulting flow </returns>
+        public static double[] stretchFlow(double[] flow, int targetLength, System.Func<double, double> modifier)
+        {
+            if (targetLength < flow.Length)
+            {
+                throw new ArgumentException("Target bucket length smaller than flow. "
+                    + "" + targetLength + " vs " + flow.Length);
+            }
+            double[] result;
+            int tempLength = targetLength;
 
-			if (flow.Length != 1 && (tempLength - flow.Length) % (flow.Length - 1) != 0)
-			{
-				tempLength = (flow.Length - 1) * (tempLength - flow.Length) + 1;
-			}
+            if (flow.Length != 1 && (tempLength - flow.Length) % (flow.Length - 1) != 0)
+            {
+                tempLength = (flow.Length - 1) * (tempLength - flow.Length) + 1;
+            }
 
-			result = new double[tempLength];
-			int insider = flow.Length - 2;
-			int stepLength = (int)((tempLength - 2) / (double)(insider + 1)) + 1;
-			int countToNextStep = stepLength;
-			int fillValueIndex = 0;
-			for (int i = 0; i < tempLength; i++)
-			{
-				double fillValueBottom = flow[fillValueIndex];
-				double fillValueTop = fillValueIndex + 1 < flow.Length ? flow[fillValueIndex + 1] : flow[fillValueIndex];
+            result = new double[tempLength];
+            int insider = flow.Length - 2;
+            int stepLength = (int)((tempLength - 2) / (double)(insider + 1)) + 1;
+            int countToNextStep = stepLength;
+            int fillValueIndex = 0;
+            for (int i = 0; i < tempLength; i++)
+            {
+                double fillValueBottom = flow[fillValueIndex];
+                double fillValueTop = fillValueIndex + 1 < flow.Length ? flow[fillValueIndex + 1] : flow[fillValueIndex];
 
-				double completion = (stepLength - countToNextStep) / (double)stepLength;
+                double completion = (stepLength - countToNextStep) / (double)stepLength;
 
-				result[i] = fillValueBottom * (1 - completion) + fillValueTop * completion;
+                result[i] = fillValueBottom * (1 - completion) + fillValueTop * completion;
 
-				countToNextStep--;
+                countToNextStep--;
 
-				if (countToNextStep == 0)
-				{
-					countToNextStep = stepLength;
-					fillValueIndex++;
-				}
-			}
+                if (countToNextStep == 0)
+                {
+                    countToNextStep = stepLength;
+                    fillValueIndex++;
+                }
+            }
 
-			if (tempLength != targetLength)
-			{
-				result = reduceFlow(result, targetLength);
-			}
+            if (tempLength != targetLength)
+            {
+                result = reduceFlow(result, targetLength);
+            }
 
-			return result.ToArray();
-			//return java.util.result.Select(modifier.apply).ToArray(); //TODO...
-		}
+            return result.ToArray();
+            //return java.util.result.Select(modifier.apply).ToArray(); //TODO...
+        }
 
-		/// <summary>
-		/// Reduction causes loss of information, so the resulting flow is always 'good enough', but is not quaranteed
-		/// to be equivalent, just a shorter version of the original flow </summary>
-		/// <param name="flow"> the original flow </param>
-		/// <param name="targetLength"> the resulting array length </param>
-		/// <returns> the resulting flow </returns>
-		public static double[] reduceFlow(double[] flow, int targetLength)
-		{
-			if (flow.Length <= targetLength)
-			{
-				throw new System.ArgumentException("Bad arguments [" + flow.Length
-					+ ", " + targetLength
-					+ "]");
-			}
+        /// <summary>
+        /// Reduction causes loss of information, so the resulting flow is always 'good enough', but is not quaranteed
+        /// to be equivalent, just a shorter version of the original flow </summary>
+        /// <param name="flow"> the original flow </param>
+        /// <param name="targetLength"> the resulting array length </param>
+        /// <returns> the resulting flow </returns>
+        public static double[] reduceFlow(double[] flow, int targetLength)
+        {
+            if (flow.Length <= targetLength)
+            {
+                throw new System.ArgumentException("Bad arguments [" + flow.Length + ", " + targetLength + "]");
+            }
 
-			double multiplier = targetLength / (double)flow.Length;
-			double[] result = new double[targetLength];
-			for (int i = 0; i < flow.Length; i++)
-			{
-				double index = (i * multiplier);
-				double untilIndex = (i + 1) * multiplier;
-				int indexInt = (int)index;
-				int untilIndexInt = (int)untilIndex;
-				if (indexInt != untilIndexInt)
-				{
-					double resultIndexPortion = 1 - (index - indexInt);
-					double nextResultIndexPortion = untilIndex - untilIndexInt;
-					result[indexInt] += flow[i] * resultIndexPortion;
-					if (untilIndexInt < result.Length)
-					{
-						result[untilIndexInt] += flow[i] * nextResultIndexPortion;
-					}
-				}
-				else
-				{
-					result[indexInt] += flow[i] * (untilIndex - index);
-				}
-			}
+            double multiplier = targetLength / (double)flow.Length;
+            double[] result = new double[targetLength];
+            for (int i = 0; i < flow.Length; i++)
+            {
+                double index = (i * multiplier);
+                double untilIndex = (i + 1) * multiplier;
+                int indexInt = (int)index;
+                int untilIndexInt = (int)untilIndex;
+                if (indexInt != untilIndexInt)
+                {
+                    double resultIndexPortion = 1 - (index - indexInt);
+                    double nextResultIndexPortion = untilIndex - untilIndexInt;
+                    result[indexInt] += flow[i] * resultIndexPortion;
+                    if (untilIndexInt < result.Length)
+                    {
+                        result[untilIndexInt] += flow[i] * nextResultIndexPortion;
+                    }
+                }
+                else
+                {
+                    result[indexInt] += flow[i] * (untilIndex - index);
+                }
+            }
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 }
