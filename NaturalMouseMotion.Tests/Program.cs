0689c181e77513f04116c779a944fa15ddbc603c
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-08-13 11:08:21 +0300

    ...

--- NaturalMouseMotion.Tests/Program.cs
+++ NaturalMouseMotion.Tests/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSNaturalMouseMotion.Util;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,17 @@ namespace NaturalMouseMotion.Tests
         static void Main(string[] args)
         {
             MouseMotionFactory factory;
-            factory = new MouseMotionFactory();
+            //factory = new MouseMotionFactory();
+
+
+            //factory = FactoryTemplates.createGrannyMotionFactory();
+            //factory = FactoryTemplates.createDemoRobotMotionFactory(100);
+            //factory = FactoryTemplates.createFastGamerMotionFactory();
+            //factory = FactoryTemplates.createAverageComputerUserMotionFactory();
+            factory = FactoryTemplates.createFastGamerMotionFactory();
+
+
+
 
             MouseMotion motion = factory.build(350, 450);
             MouseMotion motionBack = factory.build(1900, 1000);
@@ -24,6 +35,24 @@ namespace NaturalMouseMotion.Tests
                 motionBack.move();
                 Thread.Sleep(50);
             }
+
+
+            //MouseMotion motion = factory.build(350, 450);
+            //MouseMotion motionBack = factory.build(1900, 1000);
+
+            //for (int i = 0; i < 4; ++i)
+            //{
+            //    motion.move();
+            //    Thread.Sleep(50);
+            //    motionBack.move();
+            //    Thread.Sleep(50);
+            //}
+
+
+
+
+
+            Console.Read();
         }
     }
 }
